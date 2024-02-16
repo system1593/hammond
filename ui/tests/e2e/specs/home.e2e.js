0133941218c597724e81efa01d561fbebb5b5a5e
system1593 <95762080+system1593@users.noreply.github.com>
2024-02-16 01:19:45 +0100

    Update home.e2e.js
    prova midifica testo

--- ui/tests/e2e/specs/home.e2e.js
+++ ui/tests/e2e/specs/home.e2e.js
@@ -1,7 +1,7 @@
 describe('Home Page', () => {
   it('has the correct title and heading', () => {
     cy.visit('/')
-    cy.title().should('equal', 'Home | Hammond')
+    cy.title().should('equal', 'Home | Hammond-Test')
     cy.contains('h1', 'Home Page')
   })
 })
